0d3d4b888734e9f8aeeda8028e72df90c0fb6b3f
NebulaPhoenix1 <luke.robb210@gmail.com>
2025-10-28 10:44:13 +0000

    Added Traps

---
+++ Assets/Scripts/RoomContent/Trap.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trap : MonoBehaviour
+{
+    [SerializeField] private uint damageAmount = 1;
+    public UnityEvent trapPlayerDamageDealt;
+    private void OnTriggerEnter(Collider other)
+    {
+        //If other has a health component, deal damage
+        //This should repeat if entity stays in trap 
+        if(other.GetComponent<Health>() != null)
+        {
+            other.GetComponent<Health>().TakeDamage(damageAmount);
+            Debug.Log(other.gameObject.name + " has triggered a trap and taken " + damageAmount + " damage.");
+            //Invoke trap damage dealt if other is player
+            if(other.gameObject.CompareTag("Player"))
+            {
+                trapPlayerDamageDealt.Invoke();
+            }
+        }
+    }
+}
