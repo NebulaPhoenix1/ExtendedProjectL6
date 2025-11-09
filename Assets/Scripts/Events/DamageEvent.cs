9a65946753726bf104d37110c321578db566a1e7
Luke <s267071@uos.ac.uk>
2025-11-09 10:50:24 +0000

    Started damage/heal events

---
+++ Assets/Scripts/Events/DamageEvent.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+public class DamageEvent
+{
+    private uint dmgAmount;
+    private GameObject source;
+    private GameObject target;
+    private Vector3 distanceBetweenSourceAndTarget;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Events/HealEvent.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+//Events for keeping track of healing done in game for DDA later
+public class HealEvent
+{
+    private uint healAmount;
+    private GameObject source;
+    private GameObject target;
+    private Vector3 distanceBetweenSourceAndTarget;
+
+    public HealEvent(uint healAmount, GameObject source, GameObject target)
+    {
+        this.healAmount = healAmount;
+        this.source = source;
+        this.target = target;
+        distanceBetweenSourceAndTarget = target.transform.position - source.transform.position;
+    }
+}
